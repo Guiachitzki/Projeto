0d8e0a2d4e26464fe5e1c29eb2e3407183ee7e80
Guilherme Iachitzki <111025350+gciachitzki@users.noreply.github.com>
2023-07-06 23:53:55 -0300

    Add files via upload

---
+++ projetosmart-api/src/test/java/com/projetosmart/crm/ProjetosmartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosmart.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosmartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
